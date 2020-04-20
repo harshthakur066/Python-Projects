16ca19612a9f9bc237a4c573da04cbb637380cdf
harshthakur066 <harshthakur066@gmail.com>
2020-04-20 16:40:15 +0530

    fetched data from hacker news

--- Scraping Data/scrape.py
+++ Scraping Data/scrape.py
@@ -1,5 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def sort_stories_by_votes(hn_list):
+    return sorted(hn_list, key=lambda k: k['votes'])[::-1]
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, items in enumerate(links):
+        title = items.getText()
+        href = items.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'links': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+pprint.pprint(create_custom_hn(links, subtext))
